1676271b00c39815ca18da1f32399a02a2c35e5f
Vankka <vankka.main@gmail.com>
2021-06-07 14:55:36 +0300

    Fix dependency issues & a few bugs

--- src/main/java/com/discordsrv/alerts/collection/ExpiringDualHashBidiMap.java
+++ src/main/java/com/discordsrv/alerts/collection/ExpiringDualHashBidiMap.java
@@ -1,23 +1,19 @@
-/*-
- * LICENSE
- * DiscordSRV
- * -------------
- * Copyright (C) 2016 - 2021 Austin "Scarsz" Shapiro
- * -------------
+/*
+ * Alerts: A bukkit plugin to send customizable alerts to Discord driven by events and commands
+ * Copyright (C) 2021 Alerts contributors
+ *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * END
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.discordsrv.alerts.collection;
